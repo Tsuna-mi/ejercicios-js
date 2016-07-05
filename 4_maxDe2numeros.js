a4922427ab98a0c3807231cc9b7131af7acce5db
Ana Gamito <imaginarte3d@gmail.com>
2016-07-05 12:14:59 +0200

    Función que devuelve el número máximo entre 2 dados

---
+++ 4_maxDe2numeros.js
@@ -0,0 +1,10 @@
+// Define a function max() that takes two numbers as arguments and 
+// returns the largest of them. Use the if-then-else construct available in Javascript.
+
+function max (a,b){
+	if(b > a){
+		console.log("el máximo es " + b);
+	}else{
+		console.log("el máximo es " + a);
+	}
+}
