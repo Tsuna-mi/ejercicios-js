4690fdfd4c35d4620ccbaebc8a5c0a24891e349d
Ana Gamito <imaginarte3d@gmail.com>
2016-07-05 17:27:26 +0200

    Function isBoolean

---
+++ 13_isBoolean.js
@@ -0,0 +1,7 @@
+// Define a function called ​isBoolean​ that receives a value and return true 
+//if the value received is a boolean
+
+//   isString(3) // false
+//   isString("true") // false
+//   isString(true) // true
+
